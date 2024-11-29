db94149accb4cada4177293a207ed26f352266ac
Danny Tan <129268549+DannyTan8x@users.noreply.github.com>
2024-11-29 11:17:03 +0800

    initialize web server

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 // index.js
 // This is the main entry point of our application
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => res.send('Hello Node Server!!'));
+
+app.listen(port, () =>
+  console.log(`Server running at http://localhost:${port}`)
+);
